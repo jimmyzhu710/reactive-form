c408cac1af44b5748fed899f7d62c5da0be9c8b1
Jimmy <jzhu@choice.com.au>
2020-05-05 22:16:49 +1000

    203. Syncing HTML and Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     this.signupForm = new FormGroup({
       'username': new FormControl(null),
-      'emai': new FormControl(null),
+      'email': new FormControl(null),
       'gender': new FormControl('male')
     });
   }
